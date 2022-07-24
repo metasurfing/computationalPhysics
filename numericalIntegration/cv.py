2524dff35f5ef87d16a0effa365a37d4fd58ff93
Luke Szymanski <luke.j.szymanski@gmail.com>
2022-07-24 17:09:46 -0400

    Moved chapter-5 files into the directory chapter-5

--- numericalIntegration/cv.py
+++
@@ -1,8 +0,0 @@
-def cv(T ,ThetaD, V, rho, N, x, w):
-    from math import exp
-    from numInt import gaussQuadInt1D
-    kB = 1.380649*10**-23
-    def f(x):
-        return (x**4)*exp(x)/((exp(x)-1)**2)
-    int = gaussQuadInt1D(f,0,ThetaD/T,N,x,w)
-    return 9*V*rho*kB*(T/ThetaD)**3*int
