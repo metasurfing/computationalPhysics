a2c0d42a33e89ee00cb6b92ad217527b425ca3ba
Luke Szymanski <luke.j.szymanski@gmail.com>
2022-08-17 21:48:18 -0400

    Finished exercise 6.9

--- chapter-6/ex6p9.py
+++ chapter-6/ex6p9.py
@@ -1,11 +1,13 @@
 from sys import path
 path.insert(0,'/Users/lukeszymanski/Documents/python/computationalPhysics/physics-packages/')
 
-from numpy import array, sin, diag, zeros
+from numpy import array, sin, diag, zeros, linspace, sqrt
 from numpy.linalg import eigvalsh
 from math import pi
 from physical_constant import Planck_bar, LightSpeed
 from linearAlg import eigH
+from numInt import gaussQuadInt1D
+from pylab import plot, show
 
 
 e_mass = 9.1094*10**-31
@@ -34,8 +36,27 @@ for mm in range(M):
 
 
 E, V = eigH(H,tol = 10**-6)
-Esh = eigvalsh(H)
-
-print(H)
-print(Esh/eV)
 print(diag(E)/eV)
+
+def phi(x,eigV,state,L,Nx,M):
+    def wavefunction(xx):
+        phi = 0
+        for nn in range(M):
+            phi = phi + eigV[nn,M-state]*sin((nn+1)*pi/L*xx)
+        return phi
+    def integrand(xx):
+        return abs(wavefunction(xx))**2
+    normalization = gaussQuadInt1D(integrand,0,L,100)
+    phix = wavefunction(x)/sqrt(normalization)
+    return phix
+
+Nx = 101
+x = linspace(0,L,Nx)
+
+phi1 = phi(x,V,1,L,Nx,M)
+phi2 = phi(x,V,2,L,Nx,M)
+phi3 = phi(x,V,3,L,Nx,M)
+plot(x,phi1)
+plot(x,phi2)
+plot(x,phi3)
+show()
