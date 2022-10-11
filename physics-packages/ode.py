c0fa67484562d755e17904f11c0fb178faa303d9
Luke Szymanski <luke.j.szymanski@gmail.com>
2022-10-10 21:53:43 -0400

    Modified the RK4 algorithm to work on arrays

--- physics-packages/ode.py
+++ physics-packages/ode.py
@@ -1,14 +1,20 @@
-def rk4(f,t,t0, x0):
+def rk4(f,t,x0, t0):
+    from numpy import empty
+    Nt = len(t)
+    if isinstance(x0,(int, float)):
+        x_sol = empty([1,Nt],float)
+    else:
+        x_sol = empty([len(x0), Nt], float)
+
     x = x0
-    x_sol = []
-    x_sol.append(x)
-    for tt in range(1,len(t)):
+    x_sol[:,0] = x0
+    for tt in range(1,Nt):
         h = t[tt]-t[tt-1]
         k1 = h*f(x,t[tt-1])
         k2 = h*f(x+0.5*k1,t[tt-1]+0.5*h)
         k3 = h*f(x+0.5*k2,t[tt-1]+0.5*h)
         k4 = h*f(x+k3, t[tt-1]+h)
         x += 1/6*(k1 + 2*k2 + 2*k3 + k4)
-        x_sol.append(x)
+        x_sol[:,tt] = x
 
     return x_sol
