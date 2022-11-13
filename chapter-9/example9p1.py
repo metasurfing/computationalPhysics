6c5ded3755583dabd2c6f8f47a56af706487b083
Luke Szymanski <luke.j.szymanski@gmail.com>
2022-11-13 10:09:57 -0500

    Removed a redundant calculation

--- chapter-9/example9p1.py
+++ chapter-9/example9p1.py
@@ -10,6 +10,10 @@ target = 1e-6
 phi = zeros([M+1, M+1],float)
 phi[0,:] = V
 phi_prime = empty([M+1,M+1],float)
+phi_prime[0,:] = phi[0,:]
+phi_prime[M,:] = phi[M,:]
+phi_prime[:,0] = phi[:,0]
+phi_prime[:,M] = phi[:,M]
 
 #Main loop
 delta = 1.0
@@ -18,10 +22,6 @@ while delta>target:
 
     phi_prime[1:M,1:M] = 0.25*(phi[2:(M+1),1:M] + phi[0:(M-1),1:M] + \
                                 phi[1:M,2:(M+1)] + phi[1:M,0:(M-1)])
-    phi_prime[0,:] = phi[0,:]
-    phi_prime[M,:] = phi[M,:]
-    phi_prime[:,0] = phi[:,0]
-    phi_prime[:,M] = phi[:,M]
 
     delta = max(abs(phi-phi_prime))
 
